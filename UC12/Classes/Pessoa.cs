87d1200fd98d4897c23cc278ce750ef2c5a5f5c7
Felix <gabritelos@hotmail.com>
2022-12-09 19:22:09 -0300

    ip

---
+++ UC12/Classes/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC12.Classes
+{
+    internal class Pessoa
+    {
+        public string? Nome { get; set; }
+        public float Rendimento { get; set; }
+
+    }
+}
---
+++ UC12/Classes/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC12.Classes
+{
+    internal class Utils
+    {
+           public static void VerificarPastaArquivo(string caminho)
+        {
+            string pasta = caminho.Split("/")[0];
+
+            if (!Directory.Exists(pasta))
+            {
+                Directory.CreateDirectory(pasta);
+            }
+            if (!File.Exists(caminho))
+            {
+                File.Create(caminho);
+            }
+        }
+    }
+}
+
